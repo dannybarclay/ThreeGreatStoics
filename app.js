07084b563109b64d44701c464821cef56caaf40f
Danny Barclay <dannybarclay@gmail.com>
2020-07-07 14:57:57 -0400

    attempting to make nav slide out when clicking link

--- app.js
+++ app.js
@@ -22,7 +22,51 @@
     
  });
 
+ navA.addEventListener('click', () => {
+    nav.classList.toggle(".nav-links2");
 
-}
+
+
+    //Animates the links in and out...//
+navLinks.forEach((link, index)=>{
+    if (link.style.animation) {
+        link.style.animation = '';
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease-out ${index /7 + .3}s`;  
+    }
+});
+
+
+});
+
+    };
+
+    const navSlideOut = () => {
+        const burger = document.querySelector(".burger");
+        const nav = document.querySelector(".nav-links");
+        const navA = document.querySelector(".nav-links a");
+        const navLinks = document.querySelectorAll(".nav-links li");
+     
+        //Toggle the navigation out!// 
+    
+     navA.addEventListener('click', () => {
+        nav.classList.toggle(".nav-links2");
+    
+    
+    
+        //Animates the links in and out...//
+    navLinks.forEach((link, index)=>{
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+          link.style.animation = `navLinkFade 0.5s ease-out ${index /7 + .3}s`;  
+        }
+    });
+    
+    
+    });
+    
+        };
 
 navSlide();
+navSlideOut();
